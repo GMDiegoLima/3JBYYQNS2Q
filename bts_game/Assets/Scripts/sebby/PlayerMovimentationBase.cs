b3572aebf011e90e47567f49f63583a4c29a1558
Sebastião dos Santos <sbt.santos@outlook.com>
2016-05-03 18:37:37 -0300

    Primeiro Teste da movimentação do Player

---
+++ bts_game/Assets/Scripts/sebby/PlayerMovimentationBase.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.EventSystems;
+
+public class PlayerMovimentationBase : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public float valor;
+    private Transform player;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Player.h = valor;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Player.h = 0;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+     //  player = GameObject.Find("Player").GetComponent<Transform>();
+
+       
+
+    }
+
+    // Update is called once per frame
+    
+
+}
